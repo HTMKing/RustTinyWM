eb60ef7c8cbcc3da304e3a07f150109474fb8aaf
Blaise Cheaney <codercheaney@gmail.com>
2020-05-20 21:34:18 -0500

    added XButtonEvent

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 extern crate x11;
 
-
-use std::ptr;
 use std::ffi::CString;
+use std::ptr;
 
 use x11::xlib::*;
 
@@ -11,20 +10,52 @@ unsafe fn str_to_cstr(x: &str) -> *const i8 {
 }
 
 fn main() {
-    let mut dpy: *mut Display = unsafe { XOpenDisplay(ptr::null_mut()) }; 
+    let mut dpy: *mut Display = unsafe { XOpenDisplay(ptr::null_mut()) };
+    let mut start: XButtonEvent = XButtonEvent{..};
 
     if dpy == ptr::null_mut() {
         panic!("Can\'t start X server!");
     }
 
     unsafe {
-        XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(str_to_cstr("F1"))) as i32, Mod1Mask,
-            XDefaultRootWindow(dpy), 1, GrabModeAsync, GrabModeAsync);
-        XGrabButton(dpy, 1, Mod1Mask, XDefaultRootWindow(dpy), 1,
-            (ButtonPressMask|ButtonReleaseMask|PointerMotionMask) as u32, GrabModeAsync, GrabModeAsync, 0, 0);
-        XGrabButton(dpy, 3, Mod1Mask, XDefaultRootWindow(dpy), 1,
-            (ButtonPressMask|ButtonReleaseMask|PointerMotionMask) as u32, GrabModeAsync, GrabModeAsync, 0, 0);
+        XGrabKey(
+            dpy,
+            XKeysymToKeycode(dpy, XStringToKeysym(str_to_cstr("F1"))) as i32,
+            Mod1Mask,
+            XDefaultRootWindow(dpy),
+            1,
+            GrabModeAsync,
+            GrabModeAsync,
+        );
+        XGrabButton(
+            dpy,
+            1,
+            Mod1Mask,
+            XDefaultRootWindow(dpy),
+            1,
+            (ButtonPressMask | ButtonReleaseMask | PointerMotionMask) as u32,
+            GrabModeAsync,
+            GrabModeAsync,
+            0,
+            0,
+        );
+        XGrabButton(
+            dpy,
+            3,
+            Mod1Mask,
+            XDefaultRootWindow(dpy),
+            1,
+            (ButtonPressMask | ButtonReleaseMask | PointerMotionMask) as u32,
+            GrabModeAsync,
+            GrabModeAsync,
+            0,
+            0,
+        );
     }
 
+    start.subwindow = 0;
 
+    loop {
+        
+    }
 }
